2d697f649de81379c1991ca35e4d4ebf06bd8f13
nhatanh <blackheron02@gmail.com>
2022-03-11 23:01:22 +0700

    Added tests for login/logout handlers in core

---
+++ micros/core/http/publicRouter/pkg_test.go
@@ -0,0 +1,80 @@
+package publicRouter
+
+import (
+	"bridge/micros/core/config"
+	manager "bridge/service-managers"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"testing"
+)
+
+var cli *manager.HttpClient
+
+func TestMain(m *testing.M) {
+	var err error
+	config.Load()
+	//log.Init(config.Get().Structured)
+	cli, err = manager.MkHttpClient("http://localhost:8001", "")
+	if err != nil {
+		fmt.Printf("Error: %s", err.Error())
+		return
+	}
+
+	m.Run()
+}
+
+type loginResp struct {
+	Token string
+}
+
+func TestLogin(t *testing.T) {
+	resp, err := cli.PostJSON("/v1/p/login", `{"username": "root", "password": "root"}`)
+	if err != nil {
+		t.Fatalf("Error: %s", err.Error())
+	}
+	defer resp.Body.Close()
+	fmt.Println("Response: ", resp)
+
+	res, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("Error: %s", err.Error())
+	}
+
+	//cli.SetToken(res)
+	fmt.Printf("status: %s\n", resp.Status)
+	fmt.Printf("response: %s\n", string(res))
+
+	liResp := loginResp{}
+	if err = json.Unmarshal(res, &liResp); err != nil {
+		t.Fatalf("Error: %s", err.Error())
+	}
+	fmt.Printf("token: %s\n", liResp.Token)
+	cli.SetToken(liResp.Token)
+
+	var jar = cli.GetCookieJar()
+	fmt.Printf("cookie jar: %v\n\n", jar)
+}
+
+func TestLogout(t *testing.T) {
+	resp, err := cli.PostJSON("/v1/p/logout", "")
+	if err != nil {
+		t.Fatalf("Error: %s", err.Error())
+	}
+	defer resp.Body.Close()
+	fmt.Println("Response: ", resp)
+
+	cli.SetToken("")
+}
+
+// re-logout logged out session should fail
+func TestDoubleLogout(t *testing.T) {
+	TestLogin(t)
+	TestLogout(t)
+	TestLogout(t)
+}
+
+func TestLoginLogout(t *testing.T) {
+	TestLogin(t)
+	TestLogout(t)
+}
