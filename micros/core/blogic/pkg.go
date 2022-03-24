fc6c56b982e2927d813b0a3c48968fc794b22964
nhatanh <blackheron02@gmail.com>
2022-03-25 03:59:40 +0700

    Updated blogic's root package

--- micros/core/blogic/pkg.go
+++ micros/core/blogic/pkg.go
@@ -2,9 +2,13 @@ package blogic
 
 import (
 	"bridge/libs"
+	ethLogic "bridge/micros/core/blogic/eth-account"
 	userLogic "bridge/micros/core/blogic/user"
+	welLogic "bridge/micros/core/blogic/wel-account"
 	"bridge/micros/core/dao"
 	manager "bridge/service-managers"
+
+	"go.temporal.io/sdk/client"
 )
 
 type InitV struct {
@@ -13,8 +17,11 @@ type InitV struct {
 	Mailer       *manager.Mailer
 	Httpcli      *manager.HttpClient
 	TokenService libs.ITokenService
+	TemporalCli  client.Client
 }
 
 func Init(iv InitV) {
 	userLogic.Init(iv.DAOs, iv.RedisManager, iv.TokenService)
+	ethLogic.Init(iv.DAOs, iv.Mailer, iv.TemporalCli)
+	welLogic.Init(iv.DAOs, iv.Mailer, iv.TemporalCli)
 }
