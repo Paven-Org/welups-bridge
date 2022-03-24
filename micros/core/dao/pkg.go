245caa95f9a80b819ba979657a774eb740c659c7
nhatanh <blackheron02@gmail.com>
2022-03-25 03:57:09 +0700

    Updated dao's root package

--- micros/core/dao/pkg.go
+++ micros/core/dao/pkg.go
@@ -3,6 +3,7 @@ package dao
 import (
 	ethDAO "bridge/micros/core/dao/eth-account"
 	userDAO "bridge/micros/core/dao/user"
+	welDAO "bridge/micros/core/dao/wel-account"
 
 	"github.com/jmoiron/sqlx"
 )
@@ -11,11 +12,13 @@ import (
 type DAOs struct {
 	User userDAO.IUserDAO
 	Eth  ethDAO.IEthDAO
+	Wel  welDAO.IWelDAO
 }
 
 func MkDAOs(db *sqlx.DB) *DAOs {
 	return &DAOs{
 		User: userDAO.MkUserDAO(db),
 		Eth:  ethDAO.MkEthDAO(db),
+		Wel:  welDAO.MkWelDAO(db),
 	}
 }
