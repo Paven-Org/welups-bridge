3938376c01d35bec7ddb8f160dd9c1044015d07a
nhatanh <blackheron02@gmail.com>
2022-03-25 03:49:52 +0700

    Added welups model

---
+++ micros/core/model/wel.go
@@ -0,0 +1,35 @@
+package model
+
+import (
+	"fmt"
+	"time"
+)
+
+type WelAccount struct {
+	Address string
+	Prikey  string
+	Status  string
+
+	Created_at time.Time
+	Updated_at time.Time
+}
+
+const (
+	WelAccountStatusLocked = "locked"
+	WelAccountStatusOK     = "ok"
+)
+
+const (
+	WelAccountRoleUnauthorized  = "unauthorized"
+	WelAccountRoleSuperAdmin    = "super_admin"
+	WelAccountRoleManager       = "MANAGER_ROLE"
+	WelAccountRoleAuthenticator = "AUTHENTICATOR"
+)
+
+var (
+	ErrWelAccountLocked         = fmt.Errorf("Welups account locked in the internal system")
+	ErrWelNoPrikey              = fmt.Errorf("Welups account doesn't have private key stored in the system")
+	ErrWelInvalidAddress        = fmt.Errorf("Invalid Address")
+	ErrWelKeyAndAddressMismatch = fmt.Errorf("Key and address mismatch")
+	ErrWelAccountNotFound       = fmt.Errorf("Account not found in internal system")
+)
