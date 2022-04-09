0486ed833d8f7f73f6c40a5c5daff327785d15e2
nhatanh <blackheron02@gmail.com>
2022-04-10 05:04:53 +0700

    [core] Updated workflow for weleth microservice

--- micros/core/microservices/weleth/pkg.go
+++ micros/core/microservices/weleth/pkg.go
@@ -1,6 +1,7 @@
 package msweleth
 
 import (
+	"bridge/micros/weleth/model"
 	welethService "bridge/micros/weleth/temporal"
 	"bridge/service-managers/logger"
 	"time"
@@ -19,6 +20,9 @@ const (
 
 	GetEthToWelCashinByTxHash  = "GetEthToWelCashinByTxHashWF"
 	GetWelToEthCashoutByTxHash = "GetWelToEthCashoutByTxHashWF"
+
+	CreateW2ECashinClaimRequestWF  = "CreateW2ECashinClaimRequestWF"
+	CreateE2WCashoutClaimRequestWF = "CreateE2WCashoutClaimRequestWF"
 )
 
 type Weleth struct {
@@ -30,6 +34,144 @@ func MkWeleth(cli client.Client) *Weleth {
 	return &Weleth{
 		tempCli: cli,
 	}
+}
+
+func (cli *Weleth) CreateW2ECashinClaimRequestWF(ctx workflow.Context, txhash string, inTokenAddr string, userAddr string, amount string, contractVersion string) (tx model.WelCashinEthTrans, err error) {
+	log := workflow.GetLogger(ctx)
+	log.Info("[Core MSWeleth] Creating cashin claim request from wel to eth with wel's side txhash: " + txhash)
+
+	ao := workflow.ActivityOptions{
+		TaskQueue:              welethService.WelethServiceQueue,
+		ScheduleToCloseTimeout: time.Second * 60,
+		ScheduleToStartTimeout: time.Second * 60,
+		StartToCloseTimeout:    time.Second * 60,
+		HeartbeatTimeout:       time.Second * 10,
+		WaitForCancellation:    false,
+		RetryPolicy: &temporal.RetryPolicy{
+			MaximumInterval: time.Second * 15,
+			MaximumAttempts: 10,
+		},
+	}
+
+	ctx = workflow.WithActivityOptions(ctx, ao)
+
+	// call weleth
+	log.Info("[Core MSWeleth] Call weleth...")
+	res := workflow.ExecuteActivity(ctx, welethService.CreateW2ECashinClaimRequest, txhash, inTokenAddr, userAddr, amount, contractVersion)
+	if err = res.Get(ctx, &tx); err != nil {
+		log.Error("[Core MSWeleth] Error while executing activity GetWelToEthCashinByTxHash in weleth microservice", err.Error())
+		return
+	}
+
+	// process
+	// outro...
+	log.Info("[Core MSWeleth] Call weleth successfully, result: ", tx)
+
+	log.Info("[Core MSWeleth] Waiting for claim request...")
+	workflow.Go(ctx, func(_ctx workflow.Context) {
+		_log := workflow.GetLogger(_ctx)
+
+		workflow.Sleep(_ctx, time.Minute)
+
+		_ao := workflow.ActivityOptions{
+			TaskQueue:              welethService.WelethServiceQueue,
+			ScheduleToCloseTimeout: time.Second * 60,
+			ScheduleToStartTimeout: time.Second * 60,
+			StartToCloseTimeout:    time.Second * 60,
+			HeartbeatTimeout:       time.Second * 10,
+			WaitForCancellation:    false,
+			RetryPolicy: &temporal.RetryPolicy{
+				MaximumInterval: time.Second * 100,
+				MaximumAttempts: 10,
+			},
+		}
+		_ctx = workflow.WithActivityOptions(_ctx, _ao)
+
+		var _tx model.WelCashinEthTrans
+		_res := workflow.ExecuteActivity(_ctx, welethService.GetWelToEthCashinByTxHash, txhash)
+		if err := _res.Get(_ctx, &_tx); err != nil {
+			_log.Info("[Temporal BG] Error while processing pending claim request: ", err.Error())
+			return
+		}
+		if _tx.ClaimStatus == model.StatusPending { // if still pending after 1 minute
+			if err := workflow.ExecuteActivity(_ctx, welethService.UpdateClaimWelCashinEth, tx.ID, tx.ReqID, model.RequestExpired, tx.ClaimTxHash, model.StatusUnknown).Get(_ctx, nil); err != nil {
+				_log.Info("[Temporal BG] Error while processing pending claim request: ", err.Error())
+				return
+			}
+		}
+		return
+	})
+
+	return tx, nil
+}
+
+func (cli *Weleth) CreateE2WCashoutClaimRequestWF(ctx workflow.Context, txhash string, outTokenAddr string, userAddr string, amount string, contractVersion string) (tx model.EthCashoutWelTrans, err error) {
+	log := workflow.GetLogger(ctx)
+	log.Info("[Core MSWeleth] Creating cashout claim request from eth to wel with eth's side txhash: " + txhash)
+
+	ao := workflow.ActivityOptions{
+		TaskQueue:              welethService.WelethServiceQueue,
+		ScheduleToCloseTimeout: time.Second * 60,
+		ScheduleToStartTimeout: time.Second * 60,
+		StartToCloseTimeout:    time.Second * 60,
+		HeartbeatTimeout:       time.Second * 10,
+		WaitForCancellation:    false,
+		RetryPolicy: &temporal.RetryPolicy{
+			MaximumInterval: time.Second * 15,
+			MaximumAttempts: 10,
+		},
+	}
+
+	ctx = workflow.WithActivityOptions(ctx, ao)
+
+	// call weleth
+	log.Info("[Core MSWeleth] Call weleth...")
+	res := workflow.ExecuteActivity(ctx, welethService.CreateE2WCashoutClaimRequest, txhash, outTokenAddr, userAddr, amount, contractVersion)
+	if err = res.Get(ctx, &tx); err != nil {
+		log.Error("[Core MSWeleth] Error while executing activity CreateE2WCashoutClaimRequest in weleth microservice", err.Error())
+		return
+	}
+
+	// process
+	// outro...
+	log.Info("[Core MSWeleth] Call weleth successfully, result: ", tx)
+
+	log.Info("[Core MSWeleth] Waiting for claim request...")
+	workflow.Go(ctx, func(_ctx workflow.Context) {
+		_log := workflow.GetLogger(_ctx)
+
+		workflow.Sleep(_ctx, time.Minute)
+
+		_ao := workflow.ActivityOptions{
+			TaskQueue:              welethService.WelethServiceQueue,
+			ScheduleToCloseTimeout: time.Second * 60,
+			ScheduleToStartTimeout: time.Second * 60,
+			StartToCloseTimeout:    time.Second * 60,
+			HeartbeatTimeout:       time.Second * 10,
+			WaitForCancellation:    false,
+			RetryPolicy: &temporal.RetryPolicy{
+				MaximumInterval: time.Second * 100,
+				MaximumAttempts: 10,
+			},
+		}
+		_ctx = workflow.WithActivityOptions(_ctx, _ao)
+
+		var _tx model.WelCashinEthTrans
+		_res := workflow.ExecuteActivity(_ctx, welethService.GetEthToWelCashinByTxHash, txhash)
+		if err := _res.Get(_ctx, &_tx); err != nil {
+			_log.Info("[Temporal BG] Error while processing pending claim request: ", err.Error())
+			return
+		}
+		if _tx.ClaimStatus == model.StatusPending { // if still pending after 1 minute
+			if err := workflow.ExecuteActivity(_ctx, welethService.UpdateClaimEthCashoutWel, tx.ID, tx.ReqID, model.RequestExpired, tx.ClaimTxHash, model.StatusUnknown).Get(_ctx, nil); err != nil {
+				_log.Info("[Temporal BG] Error while processing pending claim request: ", err.Error())
+				return
+			}
+		}
+		return
+	})
+
+	return tx, nil
 }
 
 func (cli *Weleth) GetWelToEthCashinByTxHashWF(ctx workflow.Context, txhash string) (tx welethService.BridgeTx, err error) {
@@ -54,7 +196,7 @@ func (cli *Weleth) GetWelToEthCashinByTxHashWF(ctx workflow.Context, txhash stri
 	// call weleth
 	log.Info("[Core MSWeleth] Call weleth...")
 	res := workflow.ExecuteActivity(ctx, welethService.GetWelToEthCashinByTxHash, txhash)
-	if err = res.Get(ctx, tx); err != nil {
+	if err = res.Get(ctx, &tx); err != nil {
 		log.Error("[Core MSWeleth] Error while executing activity GetWelToEthCashinByTxHash in weleth microservice", err.Error())
 		return
 	}
@@ -85,7 +227,7 @@ func (cli *Weleth) GetEthToWelCashoutByTxHashWF(ctx workflow.Context, txhash str
 	// call weleth
 	log.Info("[Core MSWeleth] Call weleth...")
 	res := workflow.ExecuteActivity(ctx, welethService.GetEthToWelCashoutByTxHash, txhash)
-	if err = res.Get(ctx, tx); err != nil {
+	if err = res.Get(ctx, &tx); err != nil {
 		log.Error("[Core MSWeleth] Error while executing activity GetEthToWelCashoutByTxHash in weleth microservice", err.Error())
 		return
 	}
@@ -116,7 +258,7 @@ func (cli *Weleth) GetEthToWelCashinByTxHashWF(ctx workflow.Context, txhash stri
 	// call weleth
 	log.Info("[Core MSWeleth] Call weleth...")
 	res := workflow.ExecuteActivity(ctx, welethService.GetEthToWelCashinByTxHash, txhash)
-	if err = res.Get(ctx, tx); err != nil {
+	if err = res.Get(ctx, &tx); err != nil {
 		log.Error("[Core MSWeleth] Error while executing activity GetEthToWelCashinByTxHash in weleth microservice", err.Error())
 		return
 	}
@@ -147,7 +289,7 @@ func (cli *Weleth) GetWelToEthCashoutByTxHashWF(ctx workflow.Context, txhash str
 	// call weleth
 	log.Info("[Core MSWeleth] Call weleth...")
 	res := workflow.ExecuteActivity(ctx, welethService.GetWelToEthCashoutByTxHash, txhash)
-	if err = res.Get(ctx, tx); err != nil {
+	if err = res.Get(ctx, &tx); err != nil {
 		log.Error("[Core MSWeleth] Error while executing activity GetWelToEthCashoutByTxHash in weleth microservice", err.Error())
 		return
 	}
@@ -162,6 +304,10 @@ func (cli *Weleth) registerService(w worker.Worker) {
 	w.RegisterWorkflowWithOptions(cli.GetEthToWelCashoutByTxHashWF, workflow.RegisterOptions{Name: GetEthToWelCashoutByTxHash})
 	w.RegisterWorkflowWithOptions(cli.GetEthToWelCashinByTxHashWF, workflow.RegisterOptions{Name: GetEthToWelCashinByTxHash})
 	w.RegisterWorkflowWithOptions(cli.GetWelToEthCashoutByTxHashWF, workflow.RegisterOptions{Name: GetWelToEthCashoutByTxHash})
+
+	w.RegisterWorkflowWithOptions(cli.CreateW2ECashinClaimRequestWF, workflow.RegisterOptions{Name: CreateW2ECashinClaimRequestWF})
+	w.RegisterWorkflowWithOptions(cli.CreateE2WCashoutClaimRequestWF, workflow.RegisterOptions{Name: CreateE2WCashoutClaimRequestWF})
+
 }
 
 func (cli *Weleth) StartService() error {
