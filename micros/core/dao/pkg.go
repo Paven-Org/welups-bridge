62ae15f25d9be03eb1b6f0f91849d54324f30520
nhatanh <blackheron02@gmail.com>
2022-04-14 03:17:25 +0700

    [core] Added block scanned tracking table for core

--- micros/core/dao/pkg.go
+++ micros/core/dao/pkg.go
@@ -1,6 +1,7 @@
 package dao
 
 import (
+	"bridge/micros/core/dao/blockscan"
 	ethDAO "bridge/micros/core/dao/eth-account"
 	userDAO "bridge/micros/core/dao/user"
 	welDAO "bridge/micros/core/dao/wel-account"
@@ -10,15 +11,19 @@ import (
 
 // sort of a locator for DAOs
 type DAOs struct {
-	User userDAO.IUserDAO
-	Eth  ethDAO.IEthDAO
-	Wel  welDAO.IWelDAO
+	User        userDAO.IUserDAO
+	Eth         ethDAO.IEthDAO
+	Wel         welDAO.IWelDAO
+	EthBlockDAO *blockscan.EthSysDAO
+	WelBlockDAO *blockscan.WelSysDAO
 }
 
 func MkDAOs(db *sqlx.DB) *DAOs {
 	return &DAOs{
-		User: userDAO.MkUserDAO(db),
-		Eth:  ethDAO.MkEthDAO(db),
-		Wel:  welDAO.MkWelDAO(db),
+		User:        userDAO.MkUserDAO(db),
+		Eth:         ethDAO.MkEthDAO(db),
+		Wel:         welDAO.MkWelDAO(db),
+		EthBlockDAO: blockscan.MkEthSysDao(db),
+		WelBlockDAO: blockscan.MkWelSysDao(db),
 	}
 }
