cdb85a3bf515ab9087a6f383b7e8e380e148e12c
nhatanh <blackheron02@gmail.com>
2022-04-13 21:46:07 +0700

    [core] Added Claim method for export contract caller

--- micros/core/abi/wel/export.go
+++ micros/core/abi/wel/export.go
@@ -81,3 +81,54 @@ func (ex *WelExport) Withdraw(opts *CallOpts, tokenAddr string, account string, 
 
 	return res, nil
 }
+
+func (ex *WelExport) Claim(opts *CallOpts, tokenAddr string, account string, requestID *big.Int, amount *big.Int, sig []byte) (*api.TransactionExtention, error) {
+	jsonString := fmt.Sprintf(`[{"address": "%s"},{"address":"%s"},{"uint256": "%s"},{"uint256":"%s"},{"bytes":"%x"}]`, tokenAddr, account, requestID.String(), amount.String(), sig)
+	res, err := ex.cli.TriggerContract(opts.From, ex.address, "claim(address,address,uint256,uint256,bytes)", jsonString, opts.Fee_limit, opts.T_amount, "", opts.T_amount)
+	if err != nil {
+		logger.Get().Err(err).Msgf("RPC to make contract call failed")
+		return nil, err
+	}
+
+	// signing
+	logger.Get().Info().Msgf("Signing transaction...")
+
+	tx := res.Transaction
+	rawData, err := proto.Marshal(tx.GetRawData())
+	if err != nil {
+		logger.Get().Err(err).Msgf("Failed to sign transaction")
+		return nil, err
+	}
+	h256h := sha256.New()
+	h256h.Write(rawData)
+	hash := h256h.Sum(nil)
+
+	signature, err := crypto.Sign(hash, opts.Prikey)
+	if err != nil {
+		return nil, err
+	}
+	tx.Signature = append(tx.Signature, signature)
+
+	// Broadcast
+	ret, err := ex.cli.Broadcast(tx)
+	if err != nil {
+		logger.Get().Err(err).Msgf("Failed to broadcast signed transaction")
+		return nil, err
+	}
+
+	fmt.Println(ret)
+	if ret.GetCode() != api.Return_SUCCESS {
+		err = fmt.Errorf(api.ReturnResponseCode_name[int32(ret.GetCode())])
+		logger.Get().Err(err).Msgf("Failed to broadcast signed transaction")
+		return nil, err
+	}
+
+	// Optionally loop to confirm transaction right here
+	// it's better to do this inside of a workflow anyway, so kinda not compelling
+
+	// return
+	res.Transaction = tx
+	res.Result = ret
+
+	return res, nil
+}
