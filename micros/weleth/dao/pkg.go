4b0b05704856f41e258b1e7e167c002f3ce7e874
nhatanh <blackheron02@gmail.com>
2022-05-16 14:48:05 +0700

    [weleth] Updated main DAOs object

--- micros/weleth/dao/pkg.go
+++ micros/weleth/dao/pkg.go
@@ -9,6 +9,7 @@ type DAOs struct {
 	WelCashinEthTransDAO  IWelCashinEthTransDAO
 	EthCashoutWelTransDAO IEthCashoutWelTransDAO
 	EthCashinWelTransDAO  IEthCashinWelTransDAO
+	WelCashoutEthTransDAO IWelCashoutEthTransDAO
 	EthSysDAO             *ethSysDAO
 	WelSysDAO             *welSysDAO
 }
@@ -18,6 +19,7 @@ func MkDAOs(db *sqlx.DB) *DAOs {
 		WelCashinEthTransDAO:  MkWelCashinEthTransDao(db),
 		EthCashoutWelTransDAO: MkEthCashoutWelTransDao(db),
 		EthCashinWelTransDAO:  MkEthCashinWelTransDao(db),
+		WelCashoutEthTransDAO: MkWelCashoutEthTransDao(db),
 		EthSysDAO:             MkEthSysDao(db),
 		WelSysDAO:             MkWelSysDao(db)}
 }
